55d8f29eda5318cff40d40da9d8f901287b2f34e
vaibhavshettar5 <vaibhavshettar5@gmail.com>
2015-12-13 23:45:01 +0530

    jsex

---
+++ jsexample.js
@@ -0,0 +1,16 @@
+courses{
+  python{
+    dur:20,start:2,end:4}
+  c{
+    dur:15,start11,end:12}
+}
+for(course in courses)
+{
+  node.buildNode(courses[course]);
+  cont.appendChild(node);
+}
+function build Name(course){
+  var html="<h1>"+name+"</h1>"+"<div> <p>"+duration+"</p></div>";
+  var box:document.createElement("div");
+  return box;
+}
